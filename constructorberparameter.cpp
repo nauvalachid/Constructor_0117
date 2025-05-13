09b16c26e005356b59b89d88420177ee0205fedb
nauvalachid <nauvalachid@gmail.com>
2025-05-13 18:57:21 +0700

    Memanggil constructor dengan parameter

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -7,3 +7,16 @@ class mahasiswa {
     //  Constructor dengan parameter
      mahasiswa(int nim, string nama);   
 };
+
+// Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor dengan parameter terpanggil" << endl;
+    cout << "NIM :" << nim << endl;
+    cout << "Nama :" << nama << endl;
+};
+
+// Memanggil constructor dengan parameter
+int main() {
+    mahasiswa mhs(12345, "Pascal");
+    return 0;
+};
